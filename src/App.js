57e3124c0bf07807e559c67b758d3ec8e06fa2f8
DevPaw <adityarpawar212@gmail.com>
2021-10-29 20:05:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { AddTransactions } from './components/AddTransactions';
+import { Balance } from './components/Balance';
+import { Header } from './components/Header';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+
+import { GlobalProvider } from './contexts/GlobalState'
+
+function App() {
+
+  return (
+    <GlobalProvider>
+      <Header/>
+      <div className="container">
+        <Balance/>
+        <IncomeExpenses/>
+        <TransactionList/>
+        <AddTransactions/>
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
